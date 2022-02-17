bb5ef25ce766d4c28241d0f94ac06a8474d823c9
kumarshubham10021998 <96285711+kumarshubham10021998@users.noreply.github.com>
2022-02-18 00:09:39 +0530

    Morning App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  
+  Route,
+  Switch 
+  
+} from "react-router-dom"
 
 export default class App extends Component {
  
@@ -11,10 +18,36 @@ export default class App extends Component {
   
     return (
       <div>
+         
+      <Router>
+      
        <NavBar/>
-       <News pageSize={5} country="in" category="science"/>
+       
+       
+          {/* <Route exact path="/" element={<News key="general" pageSize={5} country="in" category="general"/>}>
+          <Route exact path="/entertainment" element={<News pageSize={5} country="in" category="entertainment"/>}/>
+          <Route exact path="/business" element={<News key="business" pageSize={5} country="in" category="business"/>}/>
+          <Route  path="/health" element={<News  pageSize={5} country="in" category="health"/>}/>
+          <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category="science"/>}/>
+          <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports"/>}/>
+          <Route exact path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology"/>}/> 
+          </Route> */}
+           <Switch>
+          <Route exact path="/"><News  key="general" pageSize={5} country="in" category="general"/></Route> 
+          <Route exact path="/business"><News  key="business" pageSize={5} country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News  key="entertainment" pageSize={5} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News  key="general" pageSize={5} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News  key="health" pageSize={5} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News  key="science" pageSize={5} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News  key="sports" pageSize={5} country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News  key="technology" pageSize={5} country="in" category="technology"/></Route> 
+        </Switch>
+        
+        
+       </Router>
+        
+        
       </div>
     )
   }
 }
-
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
         let{title,description,imageurl,Newsurl} = this.props;
         return (
             <div className='my-2'>
-                <div className="card" style={{width: "8rem;"}}>
+                <div className="card">
                     <img src={!imageurl?"https://m.economictimes.com/thumb/msid-89603946,width-1273,height-725,resizemode-4,imgsize-134350/fewer-china-flights-may-worsen-chip-shortage-with-port-snarled-.jpg":imageurl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
